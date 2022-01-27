ff44192bb37c9d66ce6d1ca0d84756300af04b27
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-01-27 16:02:33 -0500

    use maven instead, gradle giving me a nightmare

--- src/main/java/com/semivanilla/helpplugin/HelpPlugin.java
+++ src/main/java/com/semivanilla/helpplugin/HelpPlugin.java
@@ -1,8 +1,8 @@
-package com.semivanilla.help;
+package com.semivanilla.helpplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Help extends JavaPlugin {
+public final class HelpPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
